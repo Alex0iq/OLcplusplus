493cabfdfc0c78fba2104ba47e9e68622c2f7a7d
Alex0iq <101720509+Alex0iq@users.noreply.github.com>
2022-04-09 10:29:57 +0300

    Create Шифр Цезаря.cpp

---
+++ Шифр Цезаря.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s;
+    getline(cin, s);
+    for (int i = 0; i < s.size(); ++i)
+    {
+        if (s[i] >= 'A' && s[i] <= 'Z')
+            s[i] = 'A' + (s[i] - 'A' + 3) % 26;
+        else if (s[i] >= 'a' && s[i] <= 'z')
+            s[i] = 'a' + (s[i] - 'a' + 3) % 26;
+    }
+    cout << s << endl;
+}
