63e5e3ed5455ef0b85dda36f0638ac31d5481d6d
Александр <101720509+Alex0iq@users.noreply.github.com>
2022-05-11 07:13:32 +0300

    Update Простые числа от х до у.cpp

--- Простые числа от х до у.cpp
+++ Простые числа от х до у.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	int i;
-	bool k;
+void doSomeFuckingMagic() {
 	int from, to;
 	cin >> from >> to;
 	for (int i = from; i <= to; i++) {
-		k = 0;
+		bool k = 0;
 		int b = i / 2;
 		for (int j = 2; j <= b; j++) {
 			if (i % j == 0) {
-				k = 1;
+				k += 1;
 				if (k == 1) {
 					break;
 				}
 			}
 		}
-		if (k == 0 && i!=1) {
+		if (k == 0 && i > 1) {
 			cout << i << " ";
 		}
 	}
+}
+
+int main() {
+	doSomeFuckingMagic();
 }
