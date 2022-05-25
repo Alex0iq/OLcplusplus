0f607f021a6ee2a54735480de500eb37fc397b60
Александр <101720509+Alex0iq@users.noreply.github.com>
2022-05-25 18:38:01 +0300

    Create Проигнорировать число scanf().cpp

---
+++ Проигнорировать число scanf().cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <vector>
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "ru");
+	int a, c;
+	scanf("%d%*d%d", &a, &c);
+	cout << a << ' ' << c;
+	return 0;
+}
